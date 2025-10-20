d903eb441b1c0e2fa2f9cae2c4fab6282df7520d
kumarnihar67 <51451838+kumarnihar67@users.noreply.github.com>
2025-10-20 12:00:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const http = require("http");
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content‑Type": "text/plain" });
+  res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Hello from Node.js demo App deployed via CI/CD!\n");
 });
 
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
+
